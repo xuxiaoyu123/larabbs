95f8d05b3cdedc433914fde1f86677768a943018
xuxiaoyu123 <1341255499@qq.com>
2019-08-13 18:42:22 +0800

    认证后的消息提醒

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -34,8 +34,13 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+        //设定了所有的控制器动作都需要登录后才能访问。
         $this->middleware('auth');
+
+        //设定了 只有 verify 动作使用 signed 中间件进行认证,signed 中间件是一种由框架提供的很方便的 URL 签名认证方式
         $this->middleware('signed')->only('verify');
+
+        //对 verify 和 resend 动作做了频率限制，访问频率是 1 分钟内不能超过 6 次。
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
