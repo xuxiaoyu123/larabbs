22515a31f88ca4ed80dca96151713d0f80a53a22
xuxiaoyu123 <1341255499@qq.com>
2019-08-15 15:49:20 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
      public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //UserRequest 隐式声明，目前理解为引入文件，自动进行验证，user模型验证
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
