76eeab83ec6e9e4cd9878d7f3af0dae81553fa64
xuxiaoyu123 <1341255499@qq.com>
2019-08-16 17:39:00 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 // end
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
