f557da29dcabcaf36daf7ef1f48312d01762465b
xuxiaoyu123 <1341255499@qq.com>
2019-08-18 22:20:57 +0800

    失败的seo优化  回复数据

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,6 +5,12 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
@@ -41,5 +47,10 @@ class Topic extends Model
     {
         // 按照创建时间排序
         return $query->orderBy('created_at', 'desc');
+    }
+
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
 }
